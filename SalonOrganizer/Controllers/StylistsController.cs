e14113def87d9d66d4afd6800f5bf4ff4e69f267
Mariam Alaeddin <mariamalaeddin@gmail.com>
2020-03-20 12:42:57 -0700

    add edit functions

--- SalonOrganizer/Controllers/StylistsController.cs
+++ SalonOrganizer/Controllers/StylistsController.cs
@@ -32,5 +32,33 @@ namespace SalonOrganizer.Controllers
       _db.SaveChanges();
       return RedirectToAction("Index");
     }
+    public ActionResult Delete(int id)
+    {
+      var fireMyStylist = _db.Stylists.FirstOrDefault(stylists => stylists.StylistId == id);
+      return View(fireMyStylist);
+    }
+    [HttpPost, ActionName ("Delete")]
+    public ActionResult DeleteConfirmed(int id)
+    {
+      var fireMyStylist = _db.Stylists.FirstOrDefault(stylists => stylists.StylistId == id);
+      _db.Remove(fireMyStylist);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+    public ActionResult Edit(int id)
+    {
+      var thisStylist = _db.Stylists.FirstOrDefault(stylists => stylists.StylistId == id);
+      return View(thisStylist);
+    }
+    
+
+
+
+    // public ActionResult Details(int id)
+    // {
+    //   Stylist myStylist = _db.Stylists.FirstOrDefault(stylist => stylist.StylistId == id);
+    //   myStylist.Clients = _db.Clients.Where(client => client.StylisttId == id);
+    //   return View(myStylist);
+    // }
   }
 }
