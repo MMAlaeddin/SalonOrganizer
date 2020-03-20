bf74b6169a4f6f64244d91a92689e4bccabed473
Mariam Alaeddin <mariamalaeddin@gmail.com>
2020-03-20 09:56:00 -0700

    fix bugs

--- SalonOrganizer/Startup.cs
+++ SalonOrganizer/Startup.cs
@@ -2,7 +2,7 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
-using Microsoft.EntityFrameworkCore; // to use UseMySql
+using Microsoft.EntityFrameworkCore; 
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using SalonOrganizer.Models;
