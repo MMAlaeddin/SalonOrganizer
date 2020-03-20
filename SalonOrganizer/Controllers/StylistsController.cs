210dd76e8bbc1ed4b8dd03c72fe91a212d9c0606
Mariam Alaeddin <mariamalaeddin@gmail.com>
2020-03-20 11:39:04 -0700

    start setting up stylists files and folders and go to lunch

---
+++ SalonOrganizer/Controllers/StylistsController.cs
@@ -0,0 +1,9 @@
+using Microsoft.AspNetCore.Mvc;
+using SalonOrganizer.Models;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+
+namespace SalonOrganizer.Controllers
