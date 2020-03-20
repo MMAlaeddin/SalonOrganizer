301b5cac14f4eb35d312de99aa3767ecb3d5de51
Mariam Alaeddin <mariamalaeddin@gmail.com>
2020-03-20 10:07:59 -0700

    solving more bugs

--- SalonOrganizer/Controllers/ClientsController.cs
+++ SalonOrganizer/Controllers/ClientsController.cs
@@ -21,5 +21,16 @@ namespace SalonOrganizer.Controllers
       List<Client> model = _db.Clients.ToList();
       return View(model);
     }
+    public ActionResult Create()
+    {
+      return View();
+    }
+    [HttpPost]
+    public ActionResult Create(Client client)
+    {
+      _db.Clients.Add(client);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
   }
 }
